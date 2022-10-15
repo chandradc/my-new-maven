d0b9762686a3c0aae6aab7b4dde979272eb1fd52
chandradc <dcsekars_2000@yahoo.com>
2022-10-15 18:44:35 +0530

    updated Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -15,7 +15,7 @@ public class Greeter {
 
   //TODO: Add javadoc comment
   public String greet(String someone) {
-	System.out.println("Hai This 0702");
+    System.out.println("Hai This 0702")
     return String.format("Hello, Welcome %s!", someone);
   }
 }
